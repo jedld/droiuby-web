4d77f28c62f476d4410771a9614df5611911f36e
Joseph Emmanuel Dayo <joseph.dayo@gmail.com>
2012-09-11 00:09:39 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DroiubyWeb::Application.config.secret_token = '0eb84dc20392fe04742cfdce58a595cb2ea08c675f9d81382859ee77825f7da8e9e33282dff38888d133c8409e65b49d6298a9564a54a20fb3c19a003002c827'

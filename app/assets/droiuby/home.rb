8750522409482636bf15eef3b23cf6efc58dc30b
Joseph Dayo <joseph.dayo@gmail.com>
2012-10-10 17:58:11 +0800

    Launch the explore apps page as a new activity

--- app/assets/droiuby/home.rb
+++ app/assets/droiuby/home.rb
@@ -1,4 +1,9 @@
 def on_create
+  V('.droiuby-link-activity').each do |view|
+    view.on(:click) { |v|
+      render v.data('url')
+    }
+  end
   V('.droiuby-link').each do |view|
     view.on(:click) { |v|
       render v.data('url')

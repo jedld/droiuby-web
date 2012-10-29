60e60dd2074af8a053000ae5e39bb773379c83cc
Joseph Emmanuel Dayo <joseph.dayo@gmail.com>
2012-10-29 23:28:09 +0800

    Use asset string name instead of nubmer

--- app/models/app_asset.rb
+++ app/models/app_asset.rb
@@ -2,8 +2,19 @@ class AppAsset < ActiveRecord::Base
 
   belongs_to :app
 
+  TYPE = {script: 0, image: 1, css: 2}
   attr_accessible :uri, :asset_type, :version
 
+  def asset_type_name
+    case asset_type
+      when TYPE[:script]
+        return "script"
+      when TYPE[:image]
+        return "image"
+      when TYPE[:css]
+        return "css"
+      end
+  end
 end
 
 
